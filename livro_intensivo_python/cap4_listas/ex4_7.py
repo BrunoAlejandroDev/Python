24cbec468dec8e9b2d4b911fbdbd93b9f3ef4795
Bruno Alejandro <brunoalejandrodev@gmail.com>
2025-04-22 19:05:36 -0300

    feat exercicios de lista capitulo 4

---
+++ livro_intensivo_python/cap4_listas/ex4_7.py
@@ -0,0 +1,7 @@
+'''
+4.7– Três: Crie uma lista de múltiplos de 3, de 3 a 30. Use um laço for para exibir os números de sua lista.
+'''
+
+numeros = list(range(3, 31, 3))
+for num in numeros:
+    print(num)
