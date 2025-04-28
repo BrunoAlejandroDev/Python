899d3a28e2cd6c05cf336cd502763928ef147565
Bruno Alejandro <brunoalejandrodev@gmail.com>
2025-04-28 09:18:13 -0300

    feat exercicios tuplas: desempacotamento

---
+++ exercicios/tuplas/ex08.py
@@ -0,0 +1,9 @@
+'''
+Desempacote a tupla frutas nas variáveis primeira, segunda e terceira, e imprima cada uma.
+'''
+
+frutas = ('laranja', 'abaxaci', 'manga')
+primeira_fruta, segunda_fruta, terceira_fruta = frutas
+print(primeira_fruta)
+print(segunda_fruta)
+print(terceira_fruta)
---
+++ exercicios/tuplas/ex09.py
@@ -0,0 +1,9 @@
+'''
+Usando a tupla primeiras_frutas, desempacote os dois valores em variáveis fruta1 e fruta2 e imprima.
+'''
+
+frutas = ('banana', 'abacate', 'melao', 'abacaxi')
+primeiras_frutas = frutas[0:2]
+fruta1, fruta2 = primeiras_frutas
+print(fruta1)
+print(fruta2)
