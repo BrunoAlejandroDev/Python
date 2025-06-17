1c9e94f4e021450d1cf9cb6d9c8ceb2a13918639
Bruno Alejandro <brunoalejandrodev@gmail.com>
2025-06-17 10:36:00 -0300

    feat exercicios de logica - 17/06 (ex18 incompleto)

--- exercicios/logica/ex17.py
+++ exercicios/logica/ex17.py
@@ -1,5 +1,5 @@
 '''
-Data: 16/06/25
+Data: 17/06/25
 Exercício: Caça ao Número Primo
 Enunciado: Crie uma função que recebe um número inteiro e verifica se ele é um número primo. Um número primo é aquele que é divisível apenas por 1 e por ele mesmo. A função deve retornar True se for primo e False caso contrário.
 Exemplo:
@@ -9,3 +9,26 @@ Dicas:
 Lembre-se que o número 1 não é primo.
 Você pode usar um laço for para testar a divisibilidade do número desde 2 até a sua raiz quadrada. Se encontrar algum divisor, ele não é primo.
 '''
+import math
+
+def encontrar_num_primo(numero):
+    try:
+        #* Verificar se numero eh igual a 1
+        if numero == 1:
+            return f'O numero {numero} nao eh primo'
+        
+        #* Verificar se o numero eh igual ou menor que 0
+        if numero <= 0:
+            raise ValueError('Error. Nao sao aceitos numeros menores ou iguais a 0.')
+        
+        #* Verificar se eh primo ou nao
+        if numero > 1:
+            for i in range(2, int(math.sqrt(numero))):
+                if numero % i == 0:
+                    return f'Numero {numero} nao eh numero primo'
+            return f'O numero {numero} eh um numero primo'
+    except ValueError as ve:
+        return ve
+    
+numero = 5
+print(encontrar_num_primo(numero))
---
+++ exercicios/logica/ex18.py
@@ -0,0 +1,67 @@
+'''
+Data: 17/06/25
+Exercício: Sistema de Votação
+Enunciado: Crie um programa que simule um sistema de votação. Os candidatos são representados por números. O programa deve:
+    Receber votos (números inteiros) um a um até que o número 0 seja digitado para encerrar a votação.
+    Contabilizar os votos para cada candidato.
+    Contabilizar votos nulos (qualquer voto para um candidato inexistente, ex: número negativo) e votos em branco (use um número específico, como 99).
+Ao final, exibir um relatório com:
+    O total de votos para cada candidato.
+    O total de votos nulos e em branco.
+    O candidato vencedor.
+Exemplo de Votação: Entradas: 1, 2, 1, 3, 1, 2, 99, -5, 0 (encerra)
+'''
+
+dict_candidatos = {
+    'Carlin caba bom' : {'numero_candidato' : 1, 'votos' : 0},
+    'Chico da promessa' : {'numero_candidato' : 2, 'votos' : 0},
+    'Ana da fruta' : {'numero_candidato' : 3, 'votos' : 0},
+    'Titia leila' : {'numero_candidato' : 4, 'votos' : 0},
+}
+
+def urna_eletronica(voto, votos_nulos, votos_branco, votos_validos):
+    votos_branco = 0
+    votos_nulos = 0
+    votos_validos = 0
+
+    #* Verificar se o voto eh branco
+    if voto == 99:
+        votos_branco += 1
+        return 'Voto em branco registrado com sucesso.'
+
+    #* Verificar se o voto eh nulo
+    if voto < 0:
+        votos_nulos += 1
+        return 'Voto nulo registrado com sucesso.'
+
+    #* Verificar votos validos
+    voto_registrado = False
+    for candidato, dados in dict_candidatos.items():
+        if voto == dados['numero_candidato']:
+            dados['votos'] += 1
+            votos_validos += 1
+            voto_registrado = True
+            return 'Voto registrado com sucesso.'
+    
+    #* Verificar se dentre os votos validos teve algum valor divergente
+    if not voto_registrado:
+        votos_nulos += 1
+
+def main():
+    #* Mostrar os candidatos ao usuario
+    for nome, numero in dict_candidatos.items():
+        print(f'Nome do candidato(a): {nome} | Numero do candidato(a): {numero["numero_candidato"]}')
+
+    #* Pedir o voto do usuario
+    print('\n=== URNA ELETRONICA ===')
+    print('Regras:\n Digite o numero do candidato para realizar o voto no mesmo\n Digite 99 para voto nulo\n Digite 0 para sair\n')
+    while True:
+        try:
+            voto_usuario = int(input('Digite o numero do candidato: '))
+            if voto_usuario == 0:
+                break
+            urna_eletronica(voto_usuario)
+        except ValueError:
+            print('Entrada inválida! Digite apenas numeros inteiros.')
+
+main()
