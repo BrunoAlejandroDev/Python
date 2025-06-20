3430f5cd6dad3985540536ca3b8fdfc465c9a9be
Bruno Alejandro <brunoalejandrodev@gmail.com>
2025-06-20 13:03:08 -0300

    feat exercicio logica (incompleto)

---
+++ exercicios/logica/ex25.py
@@ -0,0 +1,19 @@
+produtos = [
+    {'nome': 'Maçã', 'categoria': 'Fruta'},
+    {'nome': 'Alface', 'categoria': 'Verdura'},
+    {'nome': 'Banana', 'categoria': 'Fruta'},
+    {'nome': 'Cenoura', 'categoria': 'Legume'},
+    {'nome': 'Tomate', 'categoria': 'Fruta'} 
+]
+
+categorias_agrupadas = {}
+for produto in produtos:
+    nome_produto = produto['nome']
+    categoria = produto['categoria']
+    
+    if not categoria in categorias_agrupadas:
+        categorias_agrupadas[categoria] = [nome_produto]
+    else:
+        categorias_agrupadas[categoria]
+    
+print(categorias_agrupadas)
