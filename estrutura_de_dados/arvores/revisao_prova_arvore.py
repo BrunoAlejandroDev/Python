d94ea81c0fd9f335c1a262c002fb3b7848c7f613
Bruno Alejandro <brunoalejandrodev@gmail.com>
2025-07-22 18:11:23 -0300

    update: criação da classe Node para representar os nós da árvore

--- estrutura_de_dados/arvores/revisao_prova_arvore.py
+++ estrutura_de_dados/arvores/revisao_prova_arvore.py
@@ -3,6 +3,10 @@ EXERCICIO ARVORE BINARIA DE BUSCA - LIVROS
 '''
 
 class Livro:
+    '''
+    Classe para representar um objeto Livro.
+    Possui código e titulo como propriedades
+    '''
     def __init__(self, codigo, titulo):
         self.codigo = codigo
         self.titulo = titulo
@@ -10,4 +14,16 @@ class Livro:
     def __str__(self):
         return f'{self.codigo} | {self.titulo}'
     
-    
+class Node:
+    '''
+    Classe para representar um nó da árvore.
+    Cada nó será responsável por armazenar um livro, além de possuir acessos aos filhos da esquerda e direita.
+    '''
+    def __init__(self, livro):
+        self.livro = livro
+        self.esquerda = None
+        self.direita = None
+        
+livro1 = Livro(1, 'Vespera')
+no = Node(livro1)
+print(no.livro.titulo)
